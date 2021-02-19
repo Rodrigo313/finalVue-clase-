c53b16e6d0c4099ade6601a5abeaaf87190560cd
Rodrigo <rodrigo@rodrigo>
2021-02-19 10:08:46 +0100

    [ADD] Equipos empezado

---
+++ src/views/Equipos.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="equipos">
+        <table>
+            <tr class="tdd" v-for="(equipos, index) in arrayEquipos" :key="index">
+                <td> {{equipos.name}}</td>
+            </tr>
+            <tr v-for="(jugadores, index) in arrayJugadores" :key="index">
+                <td>{{jugadores.name}}</td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+    data(){
+        return{
+            arrayEquipos:[],
+            arrayJugadores:[]
+        }
+    },
+     created(){
+        axios.get('http://localhost:3000/clubs'),
+        axios.get('http://localhost:3000/players')
+        .then(response =>{
+            this.arrayEquipos = response.data;
+        } )
+        .catch(response => alert("Errores: " + response.status));
+    },
+    name: 'Equipos',
+}
+</script>
+
+<style scoped>
+
+</style>
